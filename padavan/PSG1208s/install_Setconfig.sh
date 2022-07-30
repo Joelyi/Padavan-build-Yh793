d9aeaf429f8b1bca91fa51ae6ea09109b641619a
Joelyi <37099326+Joelyi@users.noreply.github.com>
2022-07-30 15:48:13 +0800

    Update install_Setconfig.sh

--- padavan/PSG1208s/install_Setconfig.sh
+++ padavan/PSG1208s/install_Setconfig.sh
@@ -2,8 +2,8 @@
 
 DESTDIR=/opt/rt-n56u
 ROOTDIR=`pwd`
+CONFIG_FILENAME=PSG1208s.config
 
-
-cp -f "${ROOTDIR}/${CONFIG_FILENAME}" "${DESTDIR}/trunk/.config"
+cp -f "${ROOTDIR}/${CONFIG_FILENAME}" "${DESTDIR}/trunk/configs/templates/${CONFIG_FILENAME}"
 cp -f "${ROOTDIR}/defaults.h" "${DESTDIR}/trunk/user/shared/defaults.h"
 cp -f "${ROOTDIR}/kernel-3.4.x.config" "${DESTDIR}/trunk/configs/boards/PSG1208/kernel-3.4.x.config"
