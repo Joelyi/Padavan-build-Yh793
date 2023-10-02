665566ec0e5702aab5934684db01a8bd9670c4ef
Joelyi <37099326+Joelyi@users.noreply.github.com>
2023-10-02 20:54:08 +0800

    Create install_Setconfig.sh

---
+++ padavan/PSG1208/install_Setconfig.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DESTDIR=/opt/rt-n56u
+ROOTDIR=`pwd`
+CONFIG_FILENAME=PSG1208.config
+
+    echo "--------------开始复制配置文件----------------------"
+cp -f "${ROOTDIR}/${CONFIG_FILENAME}" "${DESTDIR}/trunk/configs/templates/${CONFIG_FILENAME}"
+cp -f "${ROOTDIR}/defaults.h" "${DESTDIR}/trunk/user/shared/defaults.h"
+cp -f "${ROOTDIR}/board.h" "${DESTDIR}/trunk/configs/boards/PSG1208/board.h"
+	      echo "--------------复制配置文件结束------------------"
