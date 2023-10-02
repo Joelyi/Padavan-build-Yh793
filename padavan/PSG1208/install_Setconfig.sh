cb7a1e5d58f7bd5d1036d9a9d606a2c0a8a4d2c0
Joelyi <37099326+Joelyi@users.noreply.github.com>
2023-10-02 22:52:51 +0800

    Update install_Setconfig.sh

--- padavan/PSG1208/install_Setconfig.sh
+++ padavan/PSG1208/install_Setconfig.sh
@@ -5,7 +5,7 @@ ROOTDIR=`pwd`
 CONFIG_FILENAME=PSG1208.config
 
     echo "--------------开始复制配置文件----------------------"
-cp -f "${ROOTDIR}/${CONFIG_FILENAME}" "${DESTDIR}/trunk/configs/templates/${CONFIG_FILENAME}"
+#cp -f "${ROOTDIR}/${CONFIG_FILENAME}" "${DESTDIR}/trunk/configs/templates/${CONFIG_FILENAME}"
 cp -f "${ROOTDIR}/defaults.h" "${DESTDIR}/trunk/user/shared/defaults.h"
 cp -f "${ROOTDIR}/kernel-3.4.x.config" "${DESTDIR}/trunk/configs/boards/PSG1208/kernel-3.4.x.config"
 	      echo "--------------复制配置文件结束------------------"
