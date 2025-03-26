ab92be87355827d7cde5571ce840f5582aa81929
Joelyi <37099326+Joelyi@users.noreply.github.com>
2025-03-26 15:48:06 +0800

    Update install_Setconfig.sh

--- padavan/PSG1208/install_Setconfig.sh
+++ padavan/PSG1208/install_Setconfig.sh
@@ -2,8 +2,9 @@
 
 DESTDIR=/opt/rt-n56u
 ROOTDIR=`pwd`
-CONFIG_FILENAME=PSG1208_nano.config
+CONFIG_FILENAME=PSG1208.config
 
     echo "--------------开始复制配置文件----------------------"
 cp -f "${ROOTDIR}/${CONFIG_FILENAME}" "${DESTDIR}/trunk/configs/templates/${CONFIG_FILENAME}"
+cp -f "${ROOTDIR}/defaults.h" "${DESTDIR}/trunk/user/shared/defaults.h"
 	      echo "--------------复制配置文件结束------------------"
