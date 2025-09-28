8d85f65ac72febd69f233a08f8a1b5f595ebf5a7
Joelyi <37099326+Joelyi@users.noreply.github.com>
2025-09-28 17:53:54 +0800

    Update install_Setconfig.sh

--- padavan/PSG1218/install_Setconfig.sh
+++ padavan/PSG1218/install_Setconfig.sh
@@ -6,4 +6,5 @@ CONFIG_FILENAME=PSG1218.config
 
     echo "--------------开始复制配置文件----------------------"
 cp -f "${ROOTDIR}/${CONFIG_FILENAME}" "${DESTDIR}/trunk/configs/templates/${CONFIG_FILENAME}"
+cp -f "${ROOTDIR}/defaults.h" "${DESTDIR}/trunk/user/shared/defaults.h"
 	      echo "--------------复制配置文件结束------------------"
