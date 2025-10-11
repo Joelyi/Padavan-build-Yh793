a0c00a08583dd7cc477a5cbe20e5c2cafbdf1764
Joelyi <37099326+Joelyi@users.noreply.github.com>
2025-10-11 14:39:59 +0800

    Update install_Setconfig.sh

--- padavan/NEWIFI3/install_Setconfig.sh
+++ padavan/NEWIFI3/install_Setconfig.sh
@@ -2,8 +2,12 @@
 
 DESTDIR=/opt/rt-n56u
 ROOTDIR=`pwd`
+default_wifi_path="${DESTDIR}/trunk/linux-3.4.x/drivers/net/wireless/ralink"
 CONFIG_FILENAME=NEWIFI3.config
 
     echo "--------------开始复制配置文件----------------------"
 cp -f "${ROOTDIR}/defaults.h" "${DESTDIR}/trunk/user/shared/defaults.h"
+sudo rm -rf $default_wifi_path/mt76x3
+sudo rm -rf $default_wifi_path/mt76x3_ap
+cp -rf ${ROOTDIR}NEWIFI3/mt7603-4.1.2.0/. $default_wifi_path
 	      echo "--------------复制配置文件结束------------------"
