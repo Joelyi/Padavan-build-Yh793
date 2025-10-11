ee5ef6ed698b54deab5eed156a5dd95c289c93f1
Joelyi <37099326+Joelyi@users.noreply.github.com>
2025-10-11 16:28:04 +0800

    Update install_wifi.sh

--- padavan/mt7603-4.1.2.1/install_wifi.sh
+++ padavan/mt7603-4.1.2.1/install_wifi.sh
@@ -8,5 +8,5 @@ default_wifi_path="${DESTDIR}/trunk/linux-3.4.x/drivers/net/wireless/ralink"
     echo "--------------开始复制配置文件----------------------"
 sudo rm -rf $default_wifi_path/mt76x3
 sudo rm -rf $default_wifi_path/mt76x3_ap
-cp -rf ${ROOTDIR}/mt7603-4.1.2.1/. $default_wifi_path
+cp -rf ${ROOTDIR}/. $default_wifi_path
 	      echo "--------------复制配置文件结束------------------"
