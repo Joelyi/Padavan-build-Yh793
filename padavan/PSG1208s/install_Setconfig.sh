db26547c54a55d29d1618cb878cba174d800ecea
Joelyi <37099326+Joelyi@users.noreply.github.com>
2025-10-12 21:38:42 +0800

    Update install_Setconfig.sh

--- padavan/PSG1208s/install_Setconfig.sh
+++ padavan/PSG1208s/install_Setconfig.sh
@@ -5,7 +5,7 @@ ROOTDIR=`pwd`
 CONFIG_FILENAME=PSG1208s.config
 
     echo "--------------开始复制配置文件----------------------"
-cp -f "${ROOTDIR}/defaults.h" "${DESTDIR}/trunk/user/shared/defaults.h"
+#cp -f "${ROOTDIR}/defaults.h" "${DESTDIR}/trunk/user/shared/defaults.h"
 cp -f "${ROOTDIR}/board.h" "${DESTDIR}/trunk/configs/boards/PSG1208/board.h"
 cp -f "${ROOTDIR}/board.mk" "${DESTDIR}/trunk/configs/boards/PSG1208/board.mk"
 cp -f "${ROOTDIR}/kernel-3.4.x.config" "${DESTDIR}/trunk/configs/boards/PSG1208/kernel-3.4.x.config"
