e5e60ebac892cd07aeada375d2cebf06eb44e27d
Joelyi <37099326+Joelyi@users.noreply.github.com>
2025-10-20 19:35:59 +0800

    Update install_Setconfig.sh

--- padavan/NEWIFI3/install_Setconfig.sh
+++ padavan/NEWIFI3/install_Setconfig.sh
@@ -7,5 +7,5 @@ CONFIG_FILENAME=NEWIFI3.config
     echo "--------------开始复制配置文件----------------------"
 cp -f "${ROOTDIR}/${CONFIG_FILENAME}" "${DESTDIR}/trunk/configs/templates/${CONFIG_FILENAME}"
 cp -f "${ROOTDIR}/defaults.h" "${DESTDIR}/trunk/user/shared/defaults.h"
-cp -f "${ROOTDIR}/common.h" "${DESTDIR}/trunk/user/httpd/common.h
+cp -f "${ROOTDIR}/common.h" "${DESTDIR}/trunk/user/httpd/common.h"
 	      echo "--------------复制配置文件结束------------------"
