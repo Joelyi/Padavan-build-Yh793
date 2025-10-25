d599505530a42aee196c5ba11f2a859bb90456a4
Joelyi <37099326+Joelyi@users.noreply.github.com>
2025-10-25 08:24:09 +0800

    Update install_wifi.sh

--- padavan/K2-wifi/install_wifi.sh
+++ padavan/K2-wifi/install_wifi.sh
@@ -6,6 +6,8 @@ ROOTDIR=`pwd`
 default_path="${DESTDIR}/trunk/proprietary/rt_wifi"
 
     echo "--------------开始复制配置文件----------------------"
+sudo rm -rf ${DESTDIR}/trunk/linux-3.4.x/drivers/net/wireless/ralink/mt76x2_ap
+sudo rm -rf ${DESTDIR}/trunk/linux-3.4.x/drivers/net/wireless/ralink/rt2860v2_ap
 sudo rm -rf $default_path/rtpci/3.0.X.X/mt76x2
 sudo rm -rf $default_path/rtpci/3.0.X.X/mt76x2_ap
 sudo rm -rf $default_path/rtsoc\2.7.X.X/rt2860v2
