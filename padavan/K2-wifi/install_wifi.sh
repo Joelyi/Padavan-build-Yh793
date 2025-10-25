0bcedb3580911801a30ff2c2670ee4eff2b0ef51
Joelyi <37099326+Joelyi@users.noreply.github.com>
2025-10-25 08:41:55 +0800

    Add driver copy commands to install_wifi.sh
    Added commands to copy mt76x2_ap and rt2860v2_ap drivers to the destination directory.

--- padavan/K2-wifi/install_wifi.sh
+++ padavan/K2-wifi/install_wifi.sh
@@ -13,5 +13,7 @@ sudo rm -rf $default_path/rtpci/3.0.X.X/mt76x2_ap
 sudo rm -rf $default_path/rtsoc\2.7.X.X/rt2860v2
 sudo rm -rf $default_path/rtsoc\2.7.X.X/rt2860v2_ap
 sudo rm -rf $default_path/rtsoc\2.7.X.X/rt2860v2_sta
+cp -rf ${ROOTDIR}/rtpci/3.0.X.X/mt76x2_ap ${DESTDIR}/trunk/linux-3.4.x/drivers/net/wireless/ralink
+cp -rf ${ROOTDIR}/rtpci/3.0.X.X/rt2860v2_ap ${DESTDIR}/trunk/linux-3.4.x/drivers/net/wireless/ralink
 cp -rf ${ROOTDIR}/. $default_path
 	      echo "--------------复制配置文件结束------------------"
