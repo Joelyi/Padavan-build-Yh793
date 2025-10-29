31f3cce983d8ff372c45fa7daeb797f011e4a0c3
Joelyi <37099326+Joelyi@users.noreply.github.com>
2025-10-29 21:55:35 +0800

    Create install_Setconfig.sh

---
+++ padavan/NEWIFI3/httpd/install_Setconfig.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DESTDIR=/opt/rt-n56u
+ROOTDIR=`pwd`
+CONFIG_FILENAME=NEWIFI3.config
+
+    echo "--------------开始复制配置文件----------------------"
+cp -f "${ROOTDIR}/common.h" "${DESTDIR}/trunk/user/httpd/common.h"
+	      echo "--------------复制配置文件结束------------------"
